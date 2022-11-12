4e9167b54db230b3534874a260d1067f89b0524e
Kazeem Ola <achieverk2@gmail.com>
2022-11-12 22:31:40 +0100

    model3

--- utils/train_model.py
+++ utils/train_model.py
@@ -18,14 +18,15 @@ from sklearn.linear_model import LinearRegression
 train = pd.read_csv('./data/df_train.csv')
 
 y_train = train[['load_shortfall_3h']]
-X_train = train[['Madrid_wind_speed','Bilbao_rain_1h','Valencia_wind_speed']]
+train[['Valencia_pressure']] = train[['Valencia_pressure']].fillna(0)
+X_train = train[['Madrid_wind_speed','Bilbao_rain_1h','Valencia_wind_speed','Valencia_pressure']]
 
 # Fit model
-lm_regression = LinearRegression(normalize=True)
+model3 = LinearRegression(normalize=True)
 print ("Training Model...")
-lm_regression.fit(X_train, y_train)
+model3.fit(X_train, y_train)
 
 # Pickle model for use within our API
-save_path = '../assets/trained-models/load_shortfall_simple_lm_regression.pkl'
+save_path = '../assets/trained-models/model3.pkl'
 print (f"Training completed. Saving model to: {save_path}")
-pickle.dump(lm_regression, open(save_path,'wb'))
+pickle.dump(model3, open(save_path,'wb'))
